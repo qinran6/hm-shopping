57fa23ebff6d4e8e8dc6fc969df2af925fc33c80
qinran6 <3546960732@qq.com>
2026-01-27 21:44:15 +0800

    配置二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
-import Layout from '@/views/layout'
+import Layout from '@/views/layout/index.vue'
 import MyOrder from '@/views/myorder'
 import Pay from '@/views/pay'
 import ProDetail from '@/views/prodetail'
 import Search from '@/views/search/index.vue'
 import SearchList from '@/views/search/list.vue'
+import Home from '@/views/layout/home.vue'
+import Category from '@/views/layout/category.vue'
+import Cart from '@/views/layout/cart.vue'
+import User from '@/views/layout/user.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', component: Login },
-    { path: '/', component: Layout },
+    {
+      path: '/',
+      component: Layout,
+      redirect: '/home',
+      children: [
+        { path: '/home', component: Home },
+        { path: '/category', component: Category },
+        { path: '/cart', component: Cart },
+        { path: '/user', component: User }
+      ]
+    },
     { path: '/myorder', component: MyOrder },
     { path: '/pay', component: Pay },
     // 动态路由传参，确认将来是哪个商品，路由参数中携带id
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <van-tabbar v-model="active" active-color="#ee0a24" inactive-color="#000">
-      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
-      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
-      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    <div>
+      <router-view/>
+    </div>
+    <van-tabbar route v-model="active" active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item to="/home" icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/category" icon="apps-o">分类页</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="manager-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
