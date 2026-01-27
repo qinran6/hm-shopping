f139cb2e36db6a4d8fee8241fbca0f9c46859f43
qinran6 <3546960732@qq.com>
2026-01-27 17:22:44 +0800

    配置一级路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+import Layout from '@/views/layout'
+import MyOrder from '@/views/myorder'
+import Pay from '@/views/pay'
+import ProDetail from '@/views/prodetail'
+import Search from '@/views/search/index.vue'
+import SearchList from '@/views/search/list.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/', component: Layout },
+    { path: '/myorder', component: MyOrder },
+    { path: '/pay', component: Pay },
+    // 动态路由传参，确认将来是哪个商品，路由参数中携带id
+    { path: '/prodetail/: id', component: ProDetail },
+    { path: '/search', component: Search },
+    { path: '/searlist', component: SearchList }
+  ]
 })
 
 export default router
