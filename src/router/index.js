5749e44e597cd0e2a3a45bae5a05801d9aafedce
qinran6 <3546960732@qq.com>
2026-01-30 23:01:52 +0800

    设置全局前置导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Home from '@/views/layout/home.vue'
 import Category from '@/views/layout/category.vue'
 import Cart from '@/views/layout/cart.vue'
 import User from '@/views/layout/user.vue'
+import { getInfo } from '@/utils/storage'
 
 Vue.use(VueRouter)
 
@@ -35,6 +36,25 @@ const router = new VueRouter({
     { path: '/search', component: Search },
     { path: '/searlist', component: SearchList }
   ]
+})
+// 全局前置守卫 所有路由在真正到达之前都会经过全局前置守卫，只有它放行才会到达对应页面
+// to: 到哪里去，到哪里去的完整路由信息对象（路径、参数）
+// from: 从哪里来，从哪里来的完整路由信息对象（路径、参数）
+// next(): 是否放行 next() 直接放行，放行到to要去的路径；next(路径) 进行拦截，拦截到next里面配置的路径
+const authUrls = ['/pay', '/myorder']
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next)
+  if (!authUrls.includes(to.path)) {
+    next()
+    return
+  }
+  const userInfo = getInfo()
+  console.log(userInfo)
+  if (userInfo.token) {
+    next()
+  } else {
+    next('/login')
+  }
 })
 
 export default router
