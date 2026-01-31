281db00af6191b7f1653dde97a10b8365c70df8a
qinran6 <3546960732@qq.com>
2026-01-31 16:29:41 +0800

    首页的静态结构准备

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
-import { Tabbar, TabbarItem, Button, NavBar, Toast } from 'vant'
+import { Tabbar, TabbarItem, Button, NavBar, Toast, Search, Swipe, SwipeItem, Lazyload, Grid, GridItem } from 'vant'
 
+Vue.use(Grid)
+Vue.use(GridItem)
+Vue.use(Lazyload)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Search)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Button)
