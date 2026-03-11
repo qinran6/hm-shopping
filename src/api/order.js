06a95ad3b4cc47420d3e4c67a30bf50a494e8945
qinran6 <3546960732@qq.com>
2026-03-11 21:50:26 +0800

    订单管理的动态渲染-点击Tab 切换渲染

--- src/api/order.js
+++ src/api/order.js
@@ -16,16 +16,23 @@ export const checkOrder = (mode, obj) => {
 }
 // 提交订单
 export const submitOrder = (mode, remark, obj) => {
-  return request.get('/checkout/submit', {
+  return request.post('/checkout/submit', {
+    mode, // buyNow立即购买 cart购物车
+    delivery: 10, // 10快递配送 20上门自提
+    shopId: 0, // 自提门店ID
+    couponId: 0, // 优惠券ID
+    isUserPoints: 0, // 是否使用积分抵扣（1使用 0不使用）
+    payType: 10, // 10：余额支付
+    remark, // 留言
+    ...obj // 将传递过来的参数对象 动态展开
+  })
+}
+// 我的订单
+export const getMyOrder = (dataType, page) => {
+  return request.get('/order/list', {
     params: {
-      mode, // buyNow立即购买 cart购物车
-      delivery: 10, // 10快递配送 20上门自提
-      shopId: 0, // 自提门店ID
-      couponId: 0, // 优惠券ID
-      isUserPoints: 0, // 是否使用积分抵扣（1使用 0不使用）
-      payType: 10, // 10：余额支付
-      remark, // 留言
-      ...obj // 将传递过来的参数对象 动态展开
+      dataType,
+      page
     }
   })
 }
--- src/views/myorder/index.vue
+++ src/views/myorder/index.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="order">
     <van-nav-bar title="我的订单" left-arrow @click-left="$router.go(-1)" />
-    <van-tabs v-model="active">
-      <van-tab title="全部"></van-tab>
-      <van-tab title="待支付"></van-tab>
-      <van-tab title="待发货"></van-tab>
-      <van-tab title="待收货"></van-tab>
-      <van-tab title="待评价"></van-tab>
+    <van-tabs v-model="active" >
+      <van-tab title="全部" name="all"></van-tab>
+      <van-tab title="待支付" name="payment"></van-tab>
+      <van-tab title="待发货" name="delivery"></van-tab>
+      <van-tab title="待收货" name="received"></van-tab>
+      <van-tab title="待评价" name="comment"></van-tab>
     </van-tabs>
 
-    <OrderListItem></OrderListItem>
+    <OrderListItem v-for="item in list" :key="item.order_id" :item="item"></OrderListItem>
   </div>
 </template>
 
 <script>
 import OrderListItem from '@/components/OrderListItem.vue'
+import { getMyOrder } from '@/api/order'
 export default {
   name: 'MyorderIndex',
   components: {
@@ -22,7 +23,29 @@ export default {
   },
   data () {
     return {
-      active: 0
+      active: this.$route.query.dataType || 'all',
+      page: 1,
+      list: []
+    }
+  },
+  methods: {
+    async getMyorder () {
+      const { data: { list } } = await getMyOrder(this.active, this.page)
+      list.data.forEach((item) => {
+        item.total_num = 0
+        item.goods.forEach(goods => {
+          item.total_num += goods.total_num
+        })
+      })
+      this.list = list.data
+    }
+  },
+  watch: {
+    active: {
+      immediate: true,
+      handler () {
+        this.getMyorder()
+      }
     }
   }
 }
