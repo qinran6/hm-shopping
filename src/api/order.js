e043a57efe9bb093405d1fb0240a336bf892740f
qinran6 <3546960732@qq.com>
2026-03-11 19:52:54 +0800

    提交订单并支付

--- src/api/order.js
+++ src/api/order.js
@@ -14,3 +14,18 @@ export const checkOrder = (mode, obj) => {
     }
   })
 }
+// 提交订单
+export const submitOrder = (mode, remark, obj) => {
+  return request.get('/checkout/submit', {
+    params: {
+      mode, // buyNow立即购买 cart购物车
+      delivery: 10, // 10快递配送 20上门自提
+      shopId: 0, // 自提门店ID
+      couponId: 0, // 优惠券ID
+      isUserPoints: 0, // 是否使用积分抵扣（1使用 0不使用）
+      payType: 10, // 10：余额支付
+      remark, // 留言
+      ...obj // 将传递过来的参数对象 动态展开
+    }
+  })
+}
--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -82,21 +82,21 @@
 
       <!-- 买家留言 -->
       <div class="buytips">
-        <textarea placeholder="选填：买家留言（50字内）" name="" id="" cols="30" rows="10"></textarea>
+        <textarea v-model="remark" placeholder="选填：买家留言（50字内）" name="" id="" cols="30" rows="10"></textarea>
       </div>
     </div>
 
     <!-- 底部提交 -->
     <div class="footer-fixed">
       <div class="left">实付款：<span>￥{{order.orderTotalPrice}}</span></div>
-      <div class="tipsbtn">提交订单</div>
+      <div class="tipsbtn" @click="submitOrder">提交订单</div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-import { checkOrder } from '@/api/order'
+import { checkOrder, submitOrder } from '@/api/order'
 
 export default {
   name: 'PayIndex',
@@ -104,7 +104,8 @@ export default {
     return {
       goodsList: [],
       order: {},
-      personal: {}
+      personal: {},
+      remark: ''
     }
   },
   created () {
@@ -157,6 +158,23 @@ export default {
         this.order = order
         this.personal = personal
       }
+    },
+    async submitOrder () {
+      if (this.mode === 'buyNow') {
+        const res = await submitOrder(this.mode, this.remark, {
+          goodsId: this.goodsId,
+          goodsSkuId: this.goodsSkuId,
+          goodsNum: this.goodsNum
+        })
+        console.log(res)
+      } else {
+        const res = await submitOrder(this.mode, this.remark, {
+          cartIds: this.cartIds
+        })
+        console.log(res)
+      }
+      this.$toast.success('支付成功')
+      this.$router.replace('/myorder')
     }
   }
 }
